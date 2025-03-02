aefc70500b58bab8925f2a825ea93521ad78159b
d2Loysia <loysiaofficial@gmail.com>
2025-03-02 15:29:16 +0300

    Add files via upload

---
+++ data/Bot.js
@@ -0,0 +1,7 @@
+const BotData = {
+    token: "MTMwOTU5NjQ1MTkwNjUyMzIwOA.Gk2nRK.GLPsKGGUWtmfk0IwQSkgAkU9ZrUIHc1TmqkS-w",
+    owners: ["702791382661595177"],
+    clientID: "1309596451906523208"
+}
+
+module.exports = { BotData }
