8f2372203bf2edcd6b6994fa0b30eed822b5ddca
d2Loysia <loysiaofficial@gmail.com>
2025-03-02 16:37:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const Bot = require('./client/index');
 
+// Sunucu oluşturma ve proje aktivitesi sağlama.
+const express - require('express');
+const app - express();
+const port - 3000;
+// Web sunucu
+app.get('/', (req, res) =>
+res.sendStatus(200);
+));
+app.listen(port, () => {
+console.log('Sunucu ${port} numaralı bağlantı noktasında yürütül0yor.');
+});
+
 const client = new Bot();
 
 client.init();
